77490085e8a7c7e0e26b58552a75b34ddd34d1d6
Takumi Ando <takumi@t15.ch>
2022-05-10 11:49:35 +0900

    Add .wezterm.lua
    Signed-off-by: Takumi Ando <takumi@t15.ch>

---
+++ .wezterm.lua
@@ -0,0 +1,30 @@
+local wezterm = require 'wezterm';
+
+return {
+    font = wezterm.font_with_fallback({
+        "UDEV Gothic LG",
+        "JetBrains Mono",
+    }),
+    font_size = 12.0,
+
+    color_scheme = "Doom Peacock",
+    colors = {
+        selection_bg = "white",
+        selection_fg = "black",
+    },
+
+    use_fancy_tab_bar = false,
+    enable_tab_bar = false,
+    enable_scroll_bar = false,
+    tab_bar_at_bottom = true,
+
+    window_padding = {
+        left = "40px",
+        right = "40px",
+        top = "40px",
+        bottom = "40px",
+    },
+
+    enable_wayland = true,
+    exit_behavior = "Close",
+}
