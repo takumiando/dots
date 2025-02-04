111278798ec152afe67b37dc4ca5b58dc8bd6d2d
Takumi Ando <me@tmmyando.com>
2025-02-04 16:44:24 +0900

    sway: WIP
    Signed-off-by: Takumi Ando <me@tmmyando.com>

---
+++ config/sway/bar.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+im () {
+    case "$(fcitx5-remote)"    in
+        1)
+            printf "    en"
+            ;;
+        2)
+            printf "    jp"
+            ;;
+    esac
+}
+
+volume () {
+    VOL="$(pactl get-sink-volume @DEFAULT_SINK@ | grep -o '[0-9]*%' | head -n 1 2> /dev/null)"
+
+    if [ -n "$VOL"    ]; then
+        printf "    vol:%s"    "$VOL"
+    fi
+}
+
+battery () {
+    BAT=/sys/class/power_supply/BAT0
+
+    if [ ! -e "$BAT"    ]; then
+        return 1
+    fi
+
+    CAPACITY="$(cat "$BAT"/capacity)"
+    STATUS="$(cat "$BAT"/status)"
+
+    if [ "$STATUS"    != "Discharging"    ]; then
+        CAPACITY=+"$CAPACITY"
+    fi
+
+    printf "    bat:%s%%"    "$CAPACITY"
+}
+
+clock () {
+        DATETIME="$(date '+%Y-%m-%d %H:%M:%S')"
+        printf "    %s"    "$DATETIME"
+}
+
+while :
+do
+    printf "%s%s%s%s"    "$(im)"    "$(volume)"    "$(battery)"    "$(clock)"
+    sleep 0.1
+done
