1a4da08c3c30a8f902532f0c4931d02ed71f8ad2
Takumi Ando <takumi@spacecubics.com>
2025-12-03 15:44:53 +0900

    nvim: Set background as #00000 forcibly
    Signed-off-by: Takumi Ando <takumi@spacecubics.com>

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -7,3 +7,5 @@ end
 require("statusline").setup()
 
 vim.cmd[[colorscheme ashen]]
+vim.api.nvim_set_hl(0, "Normal", { bg = "#000000" })
+vim.api.nvim_set_hl(0, "LineNr", { bg = "#000000", fg = "#303030" })
